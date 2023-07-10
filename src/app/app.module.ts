ba0c45e18128b7ee8a95f4541c2f6448d959a901
DamianVH1 <josephveliz99@hotmail.com>
2023-07-10 08:32:45 -0600

    Creacion de modulos, servicios e interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
