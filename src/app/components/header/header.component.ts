822032ef2ad2b943d42afc4e0fae3dcc19b32e2c
DamianVH1 <josephveliz99@hotmail.com>
2023-07-10 21:00:47 -0600

    Creacion de navbar, funciones y estilos

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  viewCart: boolean = false;
+
+  constructor(){}
+
+
+  ngOnInit(): void {
+
+  }
+
+  onToggleCart(){
+    this.viewCart = !this.viewCart;
+  }
+
 
 }
